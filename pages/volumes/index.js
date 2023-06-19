ba61a44e2041494540064d9e3a3b2f909fd89cb1
Teresa <teresa.juengel@posteo.de>
2023-06-19 11:53:12 +0200

    overview page contents

---
+++ pages/volumes/index.js
@@ -0,0 +1,17 @@
+import { introduction } from "../../lib/data";
+import 
+
+export default function volume() {
+  return (
+    <>
+      <h1>Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <h2>All Volumes</h2>
+      <ul>
+        <li>
+          <Link></Link>
+        </li>
+      </ul>
+    </>
+  );
+}
---
+++ pages/volumes/the-fellowship-of-the-ring.js
@@ -0,0 +1,3 @@
+export default function Fellow() {
+  return <h1>The Ring</h1>;
+}

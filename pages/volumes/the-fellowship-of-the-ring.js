37803eefc2fca2841ca11f912f5ed627171ef257
Teresa <teresa.juengel@posteo.de>
2023-06-19 13:41:01 +0200

    add detail page content and volume covers

--- pages/volumes/the-fellowship-of-the-ring.js
+++ pages/volumes/the-fellowship-of-the-ring.js
@@ -1,3 +1,29 @@
-export default function Fellow() {
-  return <h1>The Ring</h1>;
+import { volumes } from "./../../lib/data";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function VolumeOne() {
+  const data = volumes.find(
+    ({ slug }) => slug === "the-fellowship-of-the-ring"
+  );
+
+  return (
+    <>
+      <Link href="./">All Volumes</Link>
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+      <ul>
+        <li>
+          {data.books[0].ordinal}: {data.books[0].title}
+        </li>
+        <li>
+          {data.books[1].ordinal}: {data.books[1].title}
+        </li>
+      </ul>
+      <Image src={data.cover} width={140} height={230} alt="Bookcover" />
+      <p>
+        <Link href="the-two-towers">Next Volume: The Two Towers</Link>
+      </p>
+    </>
+  );
 }
